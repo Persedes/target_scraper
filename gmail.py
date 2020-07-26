493f0d6e3e7039c9e800a7ec99e563f555a9854a
Michael Heiser <mheiser1@gmail.com>
2020-07-26 13:53:47 -0400

    Remove unused gmail api code

--- gmail.py
+++ gmail.py
@@ -1,12 +1,4 @@
-import base64
 from email.mime.text import MIMEText
-import pickle
-import os.path
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from googleapiclient.discovery import build
-from apiclient import errors
-from google.oauth2 import service_account
 import smtplib
 import os
 
@@ -17,19 +9,6 @@ def send_via_smtp(sender, to, message):
     server.login(sender, os.environ['APP_PASSWORD'])
     server.sendmail(sender, to, message)
     server.quit()
-
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.send']
-
-def service_account_login():
-  SCOPES = ['https://www.googleapis.com/auth/gmail.send']
-  SERVICE_ACCOUNT_FILE = 'service_key.json'
-
-  credentials = service_account.Credentials.from_service_account_file(
-          SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-  delegated_credentials = credentials.with_subject("mheiser1@gmail.com")
-  service = build('gmail', 'v1', credentials=delegated_credentials)
-  return service
 
 
 def create_message(sender, to, subject, message_text):
@@ -49,25 +28,4 @@ def create_message(sender, to, subject, message_text):
   message['from'] = sender
   message['subject'] = subject
   return message.as_string()
-
-
-def send_message(message):
-  """Send an email message.
-
-  Args:
-    service: Authorized Gmail API service instance.
-    user_id: User's email address. The special value "me"
-    can be used to indicate the authenticated user.
-    message: Message to be sent.
-
-  Returns:
-    Sent Message.
-  """
-  try:
-    message = (service.users().messages().send(userId=user_id, body=message)
-               .execute())
-    print('Message Id: %s' % message['id'])
-    return message
-  except errors.HttpError as error:
-    print('An error occurred: %s' % error)
 
